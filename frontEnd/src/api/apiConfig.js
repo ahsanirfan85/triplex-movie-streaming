e97750c3b99649f59fe0e880e2d51303b9370cf1
davincecode <hyperscript.ca@gmail.com>
2022-04-15 11:29:24 -0600

    Update style

--- frontEnd/src/api/apiConfig.js
+++ frontEnd/src/api/apiConfig.js
@@ -1,8 +1,8 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '12f3a5b30597718ac323a755f4a647a9',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+  baseUrl: "https://api.themoviedb.org/3/",
+  apiKey: "12f3a5b30597718ac323a755f4a647a9",
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
 
-export default apiConfig;
+export default apiConfig
