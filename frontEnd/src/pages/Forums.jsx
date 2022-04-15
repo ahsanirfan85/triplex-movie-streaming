839cc710c21ae8b7ffbd2e5526a1ef4bef1f9911
davincecode <hyperscript.ca@gmail.com>
2022-04-15 12:52:21 -0600

    Update forums

--- frontEnd/src/pages/Forums.jsx
+++ frontEnd/src/pages/Forums.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import Forum from '../Forum/';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const forums = () => {
-  return (
-    <>
-      <Forum />
-    </>
-  )
+import { OutlineButton } from '../components/button/Button';
+import HeroSlide from '../components/hero-slide/HeroSlide';
+import MovieList from '../components/movie-list/MovieList';
+
+import Forum from "../components/Forum"
+
+import { category, movieType } from '../api/tmdbApi';
+
+const Home = () => {
+    return (
+        <>
+            <HeroSlide/>
+            <div className="container">
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Trending Movies</h2>
+                        <Link to="/movie">
+                            <OutlineButton className="small">View more</OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.movie} type={movieType.popular}/>
+                </div>
+
+                <div className="section mb-3">
+                    <Forum />
+                </div>
+
+                
+            </div>
+        </>
+    );
 }
 
-export default forums
+export default Home;
