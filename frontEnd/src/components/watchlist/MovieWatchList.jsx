c01b6716c9eb1f94b7d2dec0351e6c5880f1927d
Mustafa Paigeer <mustafapaigeer@live.com>
2022-04-25 20:16:20 -0700

    Add on click link from watchlist to movie details page

--- frontEnd/src/components/watchlist/MovieWatchList.jsx
+++ frontEnd/src/components/watchlist/MovieWatchList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 // import { useParams } from 'react-router';
-// import { Link } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
+import { category } from '../../api/tmdbApi';
 import tmdbApi from '../../api/tmdbApi';
 import apiConfig from '../../api/apiConfig';
 
@@ -13,9 +13,10 @@ import './moviewatchlist.scss';
   const MovieWatchList = (props) => {
 
     // const { category, id } = useParams();
-  
     const [item, setItem] = useState(null);
-  
+    //const items  = props.item;
+    
+
         useEffect(() => {
           const getDetail = async () => {
             const response = await tmdbApi.detail(props.category, props.id, {params:{}});
@@ -31,13 +32,19 @@ import './moviewatchlist.scss';
                 item && (
                     <>
                         <div className="mb-3 movie-content2 container">
+                            
                             <div className="movie-content2__poster">
+                              <Link to={`/${props.category}/${item.id}`}>
                                 <div className="movie-content2__poster__img" style={{backgroundImage: `url(${apiConfig.originalImage(item.poster_path || item.backdrop_path)})`}}></div>
+                              </Link>
                             </div>
+                            
                             <div className="movie-content2__info">
+                            <Link to={`/${props.category}/${item.id}`}>
                                 <h1 className="title">
                                     {item.title || item.name}
                                 </h1>
+                                </Link>
                                 <h2 className="date" placeholder="Date Released: ">
                                     Release Date: {item.release_date || item.first_air_date}
                                 </h2>
